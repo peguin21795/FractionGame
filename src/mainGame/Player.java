193f20e7514ec0a945deb0492b55b6688cc51746
Jordan Schmerge <jschmerg@mymail.mines.edu>
2015-11-24 13:10:49 -0700

    My tests almost done.

--- src/mainGame/Player.java
+++ src/mainGame/Player.java
@@ -1,16 +1,34 @@
 package mainGame;
 public class Player implements Movable{
+	private int row;
+	private int col;
 	
-	public Player(){
+	public Player()
+	{
+		row = 0;
+		col = 0;
+	}
+	
+	public void move()
+	{
+		// should only move one space
+		// move left or right depending on keyboard input
+		// check that we don't move out of bounds
+	}
+	
+	public void shootTarget()
+	{
 		
 	}
 	
-	public void move(){
-		
+	public int getRow()
+	{
+		return row;
 	}
 	
-	public void shootTarget(){
-		
+	public int getCol()
+	{
+		return col;
 	}
 
 }
